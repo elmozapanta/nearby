94a3d16b8365b36fb6a3238be6fff01907067856
elmozapanta <elmozapanta@yahoo.com>
2025-03-13 20:17:03 +0000

    Revert configuration - handling edge cases

--- uikit/lib/constants.ts
+++ uikit/lib/constants.ts
@@ -8,5 +8,5 @@ export enum CellTypes {
   TYPE_PROGRESS = 1 << 2,
 }
 
-export const ROW_CACHE_SIZE = 5000;
-export const ROW_REUSE_SIZE = 20;
+export let ROW_CACHE_SIZE = 5000;
+export let ROW_REUSE_SIZE = 20;
