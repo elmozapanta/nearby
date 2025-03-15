6e3e904c7cbc8ad0f613878218dc6194e02c5c52
elmozapanta <elmozapanta@yahoo.com>
2025-03-15 14:24:14 +0000

    Document tests - improving performance

--- windows/broadcaster.ts
+++ windows/broadcaster.ts
@@ -12,7 +12,7 @@ export class Broadcaster {
   constructor(...els: string[]) {
     this.els = els.map(e => $(`#${e}`));
     this.onkey = this.onkey.bind(this);
-    const keys = new Set(this.els.map(el => el.dataset.key));
+    let keys = new Set(this.els.map(el => el.dataset.key));
     if (keys.size) {
       keys.forEach(key => {
         Keys.on(key, this.onkey);
@@ -26,18 +26,18 @@ export class Broadcaster {
 
   set disabled(val) {
     if (val) {
-      for (const el of this.els) {
+      for (let el of this.els) {
         el.classList.add("disabled");
       }
       return;
     }
-    for (const el of this.els) {
+    for (let el of this.els) {
       el.classList.remove("disabled");
     }
   }
 
   onkey(evt: KeyboardEvent) {
-    const { localName } = evt.target as HTMLElement;
+    let { localName } = evt.target as HTMLElement;
     if (localName === "input" || localName === "textarea") {
       return undefined;
     }
