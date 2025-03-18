a3bdbbe3e9c58f855da6a79dae43117b4c3b50d1
elmozapanta <elmozapanta@yahoo.com>
2025-03-18 02:43:32 +0000

    Revert configuration - updating docs

--- tests/test_uuid.js
+++ tests/test_uuid.js
@@ -1,7 +1,7 @@
 "use strict";
 // License: CC0 1.0
 
-var uuid = require("../lib/uuid").default;
+const uuid = require("../lib/uuid").default;
 
 function *generateSome() {
   for (let i = 0; i < 1000; ++i) {
@@ -12,8 +12,8 @@ function *generateSome() {
 describe("UUID", function() {
   it("form", function() {
     // Check for version 4, which is random except for the version bits
-    var form = /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}$/;
-    var uuids = Array.from(generateSome());
+    const form = /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}$/;
+    const uuids = Array.from(generateSome());
     expect(uuids.every(e => form.test(e))).to.be.true;
   });
 });
